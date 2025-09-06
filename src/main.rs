6595814bb4b26022f7ca9545d5af1d2e6b8a8e65
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-09-06 22:41:17 +0545

    Get the active monitors list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use x11rb::rust_connection::RustConnection;
+use x11rb::connection::Connection;
+use x11rb::protocol::randr;
+
 fn main() {
-    println!("Hello, world!");
+    let (conn, screen_num) = RustConnection::connect(None)
+                                .expect("Connection failed");
+    let screen = &conn.setup().roots[screen_num];
+
+    let monitors = randr::get_monitors(&conn, screen.root, true)
+                        .expect("Unable to get monitors")
+                        .reply()
+                        .expect("Unable to get monitors");
+
+    for monitor in monitors.monitors {
+        for &output in &monitor.outputs {
+            let info = randr::get_output_info(&conn, output, 0)
+                            .expect("Unable to get output info")
+                            .reply()
+                            .expect("Unable to get output info");
+            let name = String::from_utf8_lossy(&info.name);
+            println!("{name}");
+        }
+    }
 }
